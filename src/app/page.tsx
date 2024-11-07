58c76339948351c5b24de1e646643b3d7a30a65b
sumonDev13 <sumonmondal1530765@gmail.com>
2024-11-08 02:53:06 +0600

    refactoring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Hero from "@/components/hero/Hero";
+import Hero from '@/components/hero/Hero';
 
 export default function Home() {
   return (
     <section>
-      <Hero/>
+      <Hero />
     </section>
   );
 }
